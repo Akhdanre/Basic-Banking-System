432397fd691ca0354e719fb0b9265868ad32f865
Akeon <danakhdan12@gmail.com>
2024-03-08 13:43:27 +0700

    add error handling and try run

--- banking_system.js
+++ banking_system.js
@@ -6,14 +6,32 @@ class BankAccount {
     }
 
     deposite(value) {
+        if (!Number(value)) {
+            throw new Error("input not Number")
+        }
         this.#money += value
+
     }
 
     withdraw(value) {
+        if (!Number(value)) {
+            throw new Error("input not Number")
+        }
         this.#money -= value
     }
 
     getMoneyInfo() {
-        return "uang kamu saat ini sebesar" + this.money
+        return "uang kamu saat ini sebesar " + this.#money
     }
 }
+
+let user = new BankAccount("akhdan")
+console.log(user.name)
+try {
+
+    user.deposite("jkjdf")
+} catch (err) {
+    console.log(err.message)
+}
+
+console.log(user.getMoneyInfo())
